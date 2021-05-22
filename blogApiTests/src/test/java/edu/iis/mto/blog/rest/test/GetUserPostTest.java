07b51e0ac7cfb2cc54cbc47253706c96e43143d2
Jakub Reszka <jakub.reszka@accenture.com>
2021-05-23 00:03:11 +0200

    3.4 Added test for case when user has no posts created

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/GetUserPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/GetUserPostTest.java
@@ -1,8 +1,11 @@
 package edu.iis.mto.blog.rest.test;
+
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetUserPostTest extends FunctionalTests {
 
@@ -15,5 +18,15 @@ public class GetUserPostTest extends FunctionalTests {
               .get(GET_USER_POSTS_API, FuncTestsUtils.REMOVED_USER_ID)
               .then()
               .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    void shouldNotReturnAnyPostsWhenUserDidntCreateAny() {
+        given().header(FuncTestsUtils.REQUEST_HEADER)
+               .accept(ContentType.JSON)
+               .when()
+               .get(GET_USER_POSTS_API, FuncTestsUtils.CONFIRMED_USER_ID)
+               .then()
+               .body("size()", equalTo(0));
     }
 }
