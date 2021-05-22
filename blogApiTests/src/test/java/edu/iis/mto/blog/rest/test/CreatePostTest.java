36a0742cc37a64149b4b15d80db913445236b09e
Jakub Reszka <jakub.reszka@accenture.com>
2021-05-22 02:11:30 +0200

    3.4 Parametrized tests for creating post when user is not confirmed

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
@@ -4,6 +4,8 @@ import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static io.restassured.RestAssured.given;
 
@@ -24,6 +26,21 @@ public class CreatePostTest extends FunctionalTests {
                 .statusCode(HttpStatus.SC_CREATED)
                 .when()
                 .post(CREATE_POST_API, FuncTestsUtils.CONFIRMED_USER_ID);
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {FuncTestsUtils.NEW_USER_ID, FuncTestsUtils.REMOVED_USER_ID})
+    void shouldReturnBadRequestStatusWhenUserIsNotConfirmed(long notConfirmedUserId) {
+        JSONObject jsonObj = new JSONObject().put("entry", "default entry");
+        String jsonString = jsonObj.toString();
+
+        given().accept(ContentType.JSON)
+                .header(FuncTestsUtils.HEADER)
+                .body(jsonString)
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(CREATE_POST_API, notConfirmedUserId);
     }
 
 }
