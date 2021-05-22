89e34058a580087281945a4a648e844f85f057a2
Jakub Reszka <jakub.reszka@accenture.com>
2021-05-23 00:50:03 +0200

    3.4 Added missing user import

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -2,3 +2,4 @@
 insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'jakub.reszka@gmail.com', 'Jakub', 'Reszka')
 insert into user (account_status, email, first_name, last_name) values ('REMOVED', 'marian.kowalski@gmail.com', 'Marian', 'Kowalski')
 insert into user (account_status, email, first_name, last_name) values ('NEW', 'jan.olbracht@gmail.com', 'Jan', 'Olbracht')
+insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'one.post.man@gmail.com', 'One', 'Post')
